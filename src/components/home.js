cfe348747c1456d5b444b054c5bfa6968e9de322
Rory Letteney <rory.letteney@gmail.com>
2018-09-14 15:03:50 -0500

    Updated app.js to home.js and cleared styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='home'>
         <h1>DevCamp React Starter</h1>
         <h2>React Redux</h2>
       </div>
