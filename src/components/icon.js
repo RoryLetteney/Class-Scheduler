fc7290a2cb47fe2e43a6662c7e30beb1807573b0
Rory Letteney <rory.letteney@gmail.com>
2018-09-15 06:56:59 -0500

    Integrated font awesome

---
+++ src/components/icon.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Icon = (icon, className) => {
+  return <i className={`${icon} ${className}`} />;
+};
--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import LibraryCourse from './libraryCourse';
+import React, { Component } from "react";
+import LibraryCourse from "./libraryCourse";
 
 export default class Library extends Component {
-   render() {
-       return (
-           <div className="library">
-               <h1 className="library__title">Course Library</h1>
-               <div className="library__courses">
-                <LibraryCourse/>
-                <LibraryCourse/>
-                <LibraryCourse/>
-                </div>
-           </div>
-       )
-   }
+  render() {
+    return (
+      <div className="library">
+        <h1 className="library__title">Course Library</h1>
+        {/* <div className="library__courses"> */}
+        <LibraryCourse />
+        <LibraryCourse />
+        <LibraryCourse />
+        {/* </div> */}
+      </div>
+    );
+  }
 }
