e4bef650f735cb082a7c6d74c6fc92df64442398
Rory Letteney <rory.letteney@gmail.com>
2018-09-15 09:53:35 -0500

    Added progress tracker

--- src/components/schedule/scheduleCourse.js
+++ src/components/schedule/scheduleCourse.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class ScheduleCourse extends Component {
-   render() {
-       return (
-           <div className="schedule-course">
-               <label>{this.props.title}</label>
-           </div>
-       )
-   }
+  render() {
+    return (
+      <div className="schedule-course">
+        <label>{this.props.title}</label>
+      </div>
+    );
+  }
 }
