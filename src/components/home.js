f2b4ba320d2541c8e6c936d5cb827e7369647fee
Rory Letteney <rory.letteney@gmail.com>
2018-09-15 06:21:06 -0500

    Added library and schedule components

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Library from './library/library';
+import Schedule from './schedule/schedule';
 
 export default class Home extends Component {
   render() {
     return (
       <div className='home'>
-        <h1></h1>
+        <Library/>
+        <Schedule/>
       </div>
     );
   }
