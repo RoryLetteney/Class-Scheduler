73b3358ee4f99f723d64b85a713253a4509d09f9
Rory Letteney <rory.letteney@gmail.com>
2018-09-15 06:43:35 -0500

    Added library grid and library course grid

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import LibraryCourse from './libraryCourse';
 
 export default class Library extends Component {
    render() {
        return (
            <div className="library">
                <h1 className="library__title">Course Library</h1>
+               <div className="library__courses">
+                <LibraryCourse/>
+                <LibraryCourse/>
+                <LibraryCourse/>
+                </div>
            </div>
        )
    }
---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class LibraryCourse extends Component {
+  render() {
+    return (
+      <div className="library-course">
+        <label className="library-course__title">Problem Solving</label>
+        {/* icon component */}
+        {/* arrow component */}
+        {/* action button component */}
+        <div className="library-course__description">
+          <label>Course Description</label>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis, lorem eget facilisis lacinia, tortor mauris congue velit, non malesuada nisl lorem hendrerit mauris. Vestibulum in tellus pharetra, venenatis risus at, lacinia tortor. Suspendisse aliquam arcu metus, a rhoncus velit feugiat in.</p>
+        </div>
+      </div>
+    );
+  }
+}
