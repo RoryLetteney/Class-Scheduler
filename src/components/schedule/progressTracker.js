4b8f8f2b6499730b3d736807e154c9f9d30c001d
Rory Letteney <rory.letteney@gmail.com>
2018-09-16 11:49:24 -0500

    Finished functionality with the exception of a couple of soon to be found bugs

--- src/components/schedule/progressTracker.js
+++ src/components/schedule/progressTracker.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 export default class ProgressTracker extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.calculateProgress = this.calculateProgress.bind(this)
+  }
+
+  calculateProgress() {
+    return (this.props.amountEnrolled / this.props.amountPossible) * 100 + '%'
+  }
+
   render() {
     return (
       <div className="progress-tracker">
         <label className="progress-tracker__label">Progress Tracker</label>
-        <div className="progress-tracker__percentage">0%</div>
+        <div className="progress-tracker__percentage">{this.calculateProgress()}</div>
       </div>
     );
   }
