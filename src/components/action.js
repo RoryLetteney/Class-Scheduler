528b8ea11f5278ca413e265e61a13fb195d17970
Rory Letteney <rory.letteney@gmail.com>
2018-09-17 08:03:31 -0500

    Styled schedule course component and refactored schedule grid

--- src/components/action.js
+++ src/components/action.js
@@ -33,13 +33,5 @@ export default class Action extends Component {
         className={`${this.props.className} action`}
       />
     );
-
-    // return (
-    //     <button onClick={this.props.onClick} className={`${this.props.className} action`}>
-    //         {Icon('fas fa-plus-circle')}
-    //         {/* <i class="fas fa-times-circle"></i> */}
-    //         {Icon()}
-    //     </button>
-    // )
   }
 }
